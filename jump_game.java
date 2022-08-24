3f3f227d6c5c7b74cfbecdd37ff67217ba489a5e
ADITYA GUPTA <78367281+adityagupta0123@users.noreply.github.com>
2022-08-24 19:37:27 +0530

    Update jump_game.java

--- jump_game.java
+++ jump_game.java
@@ -1,16 +1,22 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        if( nums.length == 1)
-            return true;
-        int max =0;
-        for(int i =0 ; i< nums.length - 1 && max >= i; i++ ){
-            if( max < i + nums[i])
-                max= i + nums[i];
-            if(max >= nums.length - 1)
-                return true;
+        // if( nums.length == 1)
+        //     return true;
+        // int max =0;
+        // for(int i =0 ; i< nums.length - 1 && max >= i; i++ ){
+        //     if( max < i + nums[i])
+        //         max= i + nums[i];
+        //     if(max >= nums.length - 1)
+        //         return true;
+        // }
+        // return false;
+        
+        int goal = nums.length -1;
+        for(int i =nums.length -1; i >= 0; i-- ){
+            if( i + nums[i] >= goal)
+                goal = i;
         }
-        return false;
+        return goal == 0 ? true : false; 
         
     }
 }
-// jump mean is index + value 
