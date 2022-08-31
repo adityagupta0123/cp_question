c6dd730a60cf3591790e8084af1de29c9dbd2f46
ADITYA GUPTA <78367281+adityagupta0123@users.noreply.github.com>
2022-08-31 16:28:02 +0530

    Create _____imp___coins_change_2.java

---
+++ _____imp___coins_change_2.java
@@ -0,0 +1,14 @@
+class Solution {
+    // lc 518: the no. of = all the possiable no. (maximun)
+    
+    public int change(int amount, int[] coins) {
+        int [] ch = new int [ amount + 1];
+        ch[0] = 1;
+        for(int coin : coins) {
+            for(int i = coin ; i < amount + 1; i++){
+                ch[i] += ch[i-coin];
+            }
+        }
+        return ch[amount];
+    }
+}
