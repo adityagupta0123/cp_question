a942f20f3b9dc3884ce223bab174b494c9ece4c9
ADITYA GUPTA <78367281+adityagupta0123@users.noreply.github.com>
2022-09-06 14:46:55 +0530

    Update Power_of_Two.java

--- Power_of_Two.java
+++ Power_of_Two.java
@@ -1,5 +1,13 @@
 class Solution {
-    public boolean isPowerOfTwo(int n) {
+   public boolean isPowerOfTwo(int n) {
         return n > 0 && (n & n-1) == 0;        
     }
+    // public boolean isPowerOfTwo(int n) {
+    //     if ( n == 0 )
+    //         return false;
+    //     while ( n % 2 == 0){
+    //        n /= 2;
+    //     }
+    //     return n == 1;
+    // }
 }
