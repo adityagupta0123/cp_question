08f7816ad6ac679310d64d85e65385908d926481
ADITYA GUPTA <78367281+adityagupta0123@users.noreply.github.com>
2022-11-07 17:31:05 +0530

    Create 124_BinaryTreeMaximumPathSum.java

---
+++ 124_BinaryTreeMaximumPathSum.java
@@ -0,0 +1,18 @@
+
+class Solution {
+    int max = Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        helper(root);
+        return max;
+    }
+    public int helper( TreeNode root ){
+        if( root == null )
+        return 0;
+        int left = Math.max(helper(root.left), 0);
+        int right = Math.max(helper(root.right), 0);
+
+        max = Math.max(max, root.val + left + right);
+
+        return root.val +  Math.max(left, right);
+    }
+}
