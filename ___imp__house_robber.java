24319e2c0685c360ec07b8121f76ef0ed894b826
ADITYA GUPTA <78367281+adityagupta0123@users.noreply.github.com>
2023-02-04 19:14:24 +0530

    Update ___imp__house_robber.java

--- ___imp__house_robber.java
+++ ___imp__house_robber.java
@@ -1,19 +1,29 @@
 // imp.  interview question with different version
 class Solution {
     public int rob(int[] nums) {
-        int n = nums.length;
-        if(nums == null || n == 0)
-             return 0;
-        if(n == 1)
-             return nums[0];
-        if( n== 2)
-             return Math.max(nums[0], nums[1]);
-        int [] dp = new int[n];
-        dp[0] = nums[0];
-        dp[1] = Integer.max(nums[1], nums[0]);
-        for(int i=2; i< n ; i++)
-            dp[i] = Integer.max(nums[i] + dp[i-2], dp[i-1]);
+        // int n = nums.length;
+        // if(nums == null || n == 0)
+        //      return 0;
+        // if(n == 1)
+        //      return nums[0];
+        // if( n== 2)
+        //      return Math.max(nums[0], nums[1]);
+        // int [] dp = new int[n];
+        // dp[0] = nums[0];
+        // dp[1] = Integer.max(nums[1], nums[0]);
+        // for(int i=2; i< n ; i++)
+        //     dp[i] = Integer.max(nums[i] + dp[i-2], dp[i-1]);
         
-        return dp[n-1];
+        // return dp[n-1];
+        int include = nums[0];
+        int exclude = 0;
+        for(int i =1; i< nums.length; i++){
+            int newInclude = exclude + nums[i];
+            int newExclude = Math.max(include , exclude);
+
+            include = newInclude;
+            exclude = newExclude;
+        }
+         return Math.max(include, exclude);
     }
 }
